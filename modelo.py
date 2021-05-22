783bfa7467749c87c44a2a9cf8a6aa5f17bae9c8
Filipe Costa <77355898+costapfilipe@users.noreply.github.com>
2021-05-22 16:50:53 -0300

    Modelo -> banco de questões

---
+++ modelo.py
@@ -0,0 +1,17 @@
+questoes = [
+    #Questão 1:
+    {
+        'enunciado':'texto do enunciado',
+        'opcoes':[
+            'texto da letra A',
+            'texto da letra B',
+            'texto da letra C',
+            'texto da letra D',
+            'texto da letra E',
+        ],
+        'resposta':'letra ou posicao na lista opcoes -> A ou 0',
+        'explicacao':'texto da explicacao',
+        'pontos':7,
+        'nivel':'F'
+    }
+]
