9e0f35e142885f81725ee5630315fdc922aba1c0
MatheusFarias03 <83461020+MatheusFarias03@users.noreply.github.com>
2021-05-22 16:01:18 -0300

    Create jogo_quiz_algoritmos.py

---
+++ jogo_quiz_algoritmos.py
@@ -0,0 +1,26 @@
+import random
+
+contador = 100
+nivel = 0
+acertos = []
+erros = []
+questoes = {}
+
+def resposta_certa(resposta):
+    acertos.append(resposta)
+
+def resposta_errada(resposta):
+    erros.append(resposta)
+    if nivel == 'F':
+        contador -= 20
+    elif nivel == 'M':
+        contador -= 7
+    elif nivel == 'D':
+        contador -= 4
+
+def mostrar_acertos():
+    print ('Acertos:', *acertos, sep = ', ')
+
+def mostrar_erros():
+    print('Erros:', *erros, sep = ', ')
+
